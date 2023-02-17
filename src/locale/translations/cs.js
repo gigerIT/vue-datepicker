de9b3ff5e71dd72437ebeadb128623b6975fcf57
Manu <info@gigeirt.ch>
2023-02-17 10:02:24 +0100

    adds locale cs

--- src/locale/translations/cs.js
+++ src/locale/translations/cs.js
@@ -1,9 +1,9 @@
 const localeObject = {
   name: 'cs',
-  weekdays: 'Neděle_Pondělí_Úterý_Středa_Čtvrtek_Pátek_Sobota'.split(''),
-  weekdaysShort: 'Ne_Po_Út_St_Čt_Pá_So'.split(''),
-  weekdaysMin: 'Ne_Po_Út_St_Čt_Pá_So'.split(''),
-  months: 'Leden_Únor_Březen_Duben_Květen_Červen_Červenec_Srpen_Září_Říjen_Listopad_Prosince'.split(''),
+  weekdays: 'Neděle_Pondělí_Úterý_Středa_Čtvrtek_Pátek_Sobota'.split('_'),
+  weekdaysShort: 'Ne_Po_Út_St_Čt_Pá_So'.split('_'),
+  weekdaysMin: 'Ne_Po_Út_St_Čt_Pá_So'.split('_'),
+  months: 'Leden_Únor_Březen_Duben_Květen_Červen_Červenec_Srpen_Září_Říjen_Listopad_Prosince'.split('_'),
   monthsShort: 'Led_Úno_Bře_Dub_Kvě_Čer_Čvc_Srp_Zář_Říj_Lis_Pro'.split('_'),
   weekStart: 0,
   formats: {
